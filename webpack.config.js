b719a07dd00661cfee7115f76d04cf60b2001b04
Ross Carter <indovinachi99@gmail.com>
2023-11-01 19:00:01 +0300

    delete rules url-loader svg

--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,6 @@ const config = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
     ],
   },
   plugins: [
